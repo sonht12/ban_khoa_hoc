fd95fbaf378dd5c2c532c41af9484b6c95ebee15
sonht12 <son0379878379@gmail.com>
2023-07-30 23:36:20 +0700

    update base

---
+++ src/app/rootReduces.tsx
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import productReducer from '../features/product/productSlice';
+import cartReducer from '../features/cart/cartSlice';
+import authSlice from '../features/auth/authSlice';
+
+
+const rootReducer = combineReducers({
+    product: productReducer,
+    cart: cartReducer,
+    auth: authSlice
+});
+export default rootReducer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import { useSelector } from "react-redux"
+
+const Header = () => {
+    
+}
+
+export default Header
