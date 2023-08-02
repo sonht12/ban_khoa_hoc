94d6533e728f40b93d0080ba8855fe96f75d7b60
sonht12 <son0379878379@gmail.com>
2023-08-03 01:29:55 +0700

    SignUp

--- Router/user.js
+++ Router/user.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { GetAllUser, GetOneUser, Login, SignUp,DeleteUser } from "../controllers/user";
+import { GetAllUser,forgotPassword, GetOneUser, Login, SignUp,DeleteUser, resetPassword } from "../controllers/user";
 
 const Router = express.Router();
 Router.post("/SignUp", SignUp);
 Router.post("/Signin", Login);
 Router.get("/user", GetAllUser);
 Router.get("/user/:id", GetOneUser);
-Router.delete("/user/:id", DeleteUser)
+Router.delete("/user/:id", DeleteUser);
+Router.post("/forgotPassword", forgotPassword);
+Router.post("/resetPassword", resetPassword);
 export default Router;
--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,15 @@
 import UserCheme from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
+import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+import bodyParser from 'body-parser';
 import { CheckvalidateSignIn, CheckvalidateSignUp } from "../middlewares/User";
+import ResetPasswordSchema from "../models/resetPassword";
 
-import nodemailer from 'nodemailer'
+import nodemailer  from 'nodemailer'
+import user from "../models/user";
+
 export const SignUp = async (req, res) => {
   try {
     const { name, password, email  } = req.body;
@@ -59,7 +64,6 @@ export const SignUp = async (req, res) => {
     });
   }
 };
-
 export const Login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -94,6 +98,63 @@ export const Login = async (req, res) => {
     });
   }
 };
+export const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    // check email
+    const user = await UserCheme.findOne({ email });
+    if (!user) {
+      return res.json({
+        message: "Email không tồn tại",
+      });
+    }
+    // dich vu gmail cua minh
+   const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: 'son01247662388@gmail.com',// tài khoản của mình
+        pass: 'ildsabobxdxzyzio' // mật khẩu của mình 01247662388
+      }
+    });
+    const resetToken = uuidv4();
+    user.resetToken = resetToken;
+
+    const mailOptions = {
+      from: 'son01247662388@gmail.com',
+      to: email,
+      subject: 'Password Reset',
+      text: `Click the following link to reset your password: http://son01247662388@gmail.com/reset-password/${resetToken}`,
+    };
+    transporter.sendMail(mailOptions, (error) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        return res.status(500).json({ message: 'Error sending email' });
+      }
+  
+      return res.json({ message: 'Email sent successfully' });
+    });
+  } catch (error) {
+    return res.status(400).json({
+      massage: error.message,
+    });
+  }
+};
+export const resetPassword = async (req, res) =>{
+  const { email, token, newPassword } = req.body;
+  const user = user.find((user) => user.email === email && user.resetToken === token);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found or invalid token' });
+  }
+
+  // Update the user's password and clear the reset token
+  user.password = newPassword;
+  user.resetToken = null;
+
+  return res.json({ message: 'Password reset successful' });
+}
+
+
 export const GetOneUser = async (req, res, next) => {
   try {
     const data = await UserCheme.findById(req.params.id);
