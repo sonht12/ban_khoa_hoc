a952c66f8a2c5dd1458815c807befa019ab2ef98
school2003 <117899070+ngoductruong@users.noreply.github.com>
2023-08-04 13:36:56 +0700

    feat/product_category-refs-BKH-5

---
+++ models/product.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const ProductSchema = new mongoose.Schema(
+  {
+   name:{
+    type:String,
+   },
+   price:{
+    type:String,
+   },
+   categoryId:{
+    type:mongoose.Types.ObjectId,
+    ref:"Category",
+   },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export default mongoose.model("Product",ProductSchema);
