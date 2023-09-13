ebac25dedc246855403cb7c566f16cac95bbb8f4
damvn14 <damvn14@gmail.com>
2023-09-13 05:09:21 -0700

    update
    update send email

--- src/pages/client/Contact.tsx
+++ src/pages/client/Contact.tsx
@@ -1,4 +1,4 @@
-
+// Contact 
 
 const Contact = () => {
   return (
@@ -14,22 +14,34 @@ const Contact = () => {
                     <div
                         className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
                         <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Nhận xét</h2>
-                        <p className="leading-relaxed mb-5 text-gray-600">Nêu cảm nhận của bạn sau khi hoàn thành khóa học .
+                        <p className="leading-relaxed mb-5 text-gray-600">Phản hồi về trải nghiệm của bạn cho chúng tôi!.
                         </p>
+                        <form action="https://formspree.io/f/mvojqgyr" method="POST">
+                        <div className="relative mb-4">
+                            <label className="leading-7 text-sm text-gray-600">Name</label>
+                            <input type="text" id="name" name="Name"
+                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                        </div>
                         <div className="relative mb-4">
                             <label className="leading-7 text-sm text-gray-600">Email</label>
                             <input type="email" id="email" name="email"
                                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                         </div>
+                        {/* <div className="relative mb-4">
+                            <label className="leading-7 text-sm text-gray-600">Image</label>
+                            <input type="file" id="image" name="Image"
+                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                        </div> */}
                         <div className="relative mb-4">
                             <label  className="leading-7 text-sm text-gray-600">Message</label>
                             <textarea id="message" name="message"
                                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                         </div>
-                        <button
+                        <button type="submit"
                             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Gửi</button>
                         <p className="text-xs text-gray-500 mt-3">Cảm ơn quý khách đã sử dụng dịch vụ của chúng tôi. Quý khách vui lòng đợi phản hồi trong giây lát!
                         </p>
+                        </form>
                     </div>
                 </div>
             </section>
