1d901d8bf08b7d4622df1ff9c90803b9d9c89322
sonht12 <son0379878379@gmail.com>
2023-10-02 10:47:06 +0700

    feat/phanquyen-refs-BKH-17

--- src/components/Layouts/Signin.tsx
+++ src/components/Layouts/Signin.tsx
@@ -4,6 +4,7 @@ import { IUsers } from "@/interface/user";
 import { useNavigate } from "react-router-dom";
 import { Button, Checkbox, Form, Input } from 'antd';
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { useDispatch } from 'react-redux';
 type FieldType = {
     email?: string;
     password?: string;
@@ -11,10 +12,25 @@ type FieldType = {
 const Signin = () => {
     const [signin, {isLoading}]= useLoginMutation();
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const SET_USER = 'SET_USER';
+
+    function setUser(user:IUsers) {
+      return {
+        type: SET_USER,
+        payload: user
+      };
+    }
     const onFinish = (values: IUsers) => {
         signin(values)
-            .unwrap()
-            // .then(() => navigate("/"));
+        .unwrap()
+        .then((user) => {
+            // Lưu dữ liệu người dùng vào localStorage
+            localStorage.setItem('userInfo', JSON.stringify(user));
+            // Lưu dữ liệu người dùng vào Redux
+            dispatch(setUser(user));
+            navigate("/");
+        });
 }; return (
     <div className="h-screen font-sans bg-cover bg-no-repeat bg-[url('https://khoinguonsangtao.vn/wp-content/uploads/2022/11/hinh-anh-binh-minh.jpg')]">
            <header className="mb-4">
@@ -58,60 +74,6 @@ const Signin = () => {
   </div>
 
         )
-    // return (
-    //     <>
-    //         <div
-    //             className="h-screen font-sans bg-cover bg-no-repeat bg-[url('https://khoinguonsangtao.vn/wp-content/uploads/2022/11/hinh-anh-binh-minh.jpg')]">
-    //             <div className="container mx-auto h-full flex flex-1 justify-center items-center">
-    //                 <div className="w-full max-w-lg">
-    //                     <div className="leading-loose">
-    //                         <form className="max-w-sm m-4 p-10 bg-white bg-opacity-30 rounded shadow-xl">
-    //                             <p className="text-black text-center text-lg font-bold">SIGN IN</p>
-    //                             <div className="">
-    //                                 <label className="block text-lg font-semibold text-black" htmlFor="email">E-mail</label>
-    //                                 <input
-    //                                     className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded focus:outline-none focus:bg-white"
-    //                                     type="email" placeholder="Nhập email của bạn" />
-    //                             </div>
-    //                             <div className="mt-2">
-    //                                 <label className="block  text-lg font-semibold text-black">Mật khẩu</label>
-    //                                 <input
-    //                                     className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded focus:outline-none focus:bg-white"
-    //                                     type="password" placeholder="Nhập mật khẩu của bạn" />
-    //                             </div>
-    //                             <div className="my-5">
-    //                                 <button className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 bg-gray-200 hover:bg-white hover:shadow transition duration-150">
-    //                                     <img src="https://www.svgrepo.com/show/355037/google.svg" className="w-6 h-6" alt="" /> <span>Login with Google</span>
-    //                                 </button>
-    //                             </div>
-    //                             <div className="mt-4 items-center justify-between space-x-5">
-    //                                 <button
-    //                                     className="px-4 py-1 text-white font-normal tracking-wider bg-blue-900 hover:bg-blue-500 rounded"
-    //                                     type="submit">Đăng nhập</button>
-
-    //                                 <Link to="/signup">
-    //                                     <button
-    //                                         className="px-4 py-1 text-white font-normal tracking-wider bg-blue-900 hover:bg-blue-500 rounded"
-    //                                         type="submit">Đăng ký
-    //                                     </button>
-    //                                 </Link>
-
-    //                                 <a className="inline-block items-center right-0 align-baseline font-bold text-sm text-500 text-black hover:text-gray-300"
-    //                                     href="#">Quên mật khẩu ?</a>
-    //                             </div>
-    //                             <div className="button-dangky">
-
-    //                             </div>
-    //                         </form>
-
-
-    //                     </div>
-    //                 </div>
-
-    //             </div>
-    //         </div>
-    //     </>
-    // )
 }
 
 export default Signin
--- src/components/Layouts/Signup.tsx
+++ src/components/Layouts/Signup.tsx
@@ -1,117 +1,113 @@
-import { Link } from "react-router-dom"
-import userApi, { useLoginMutation } from '@/Api/userApi';
-import { IUsers } from "@/interface/user";
+import userApi, { useSignUpMutation } from '@/Api/userApi';
 import { useNavigate } from "react-router-dom";
-import { Button, Checkbox, Form, Input } from 'antd';
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { IUsers } from '@/interface/user';
+import React from 'react';
+import { Button, Checkbox, Form, Input, Select } from 'antd';
+import { Option } from 'antd/es/mentions';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 type FieldType = {
+    name?: string;
     email?: string;
+    phoneNumber: number;
     password?: string;
+    confirmPassword?: string;
   };
-const Signin = () => {
-    const [signin, {isLoading}]= useLoginMutation();
-    const navigate = useNavigate();
-    const onFinish = (values: IUsers) => {
-        signin(values)
-            .unwrap()
-            // .then(() => navigate("/"));
+//   const prefixSelector = (
+//     <Form.Item name="prefix" noStyle>
+//       <Select style={{ width: 70 }}>
+//         <Option value="86">+86</Option>
+//         <Option value="87">+87</Option>
+//       </Select>
+//     </Form.Item>
+//   );
+const Signup = () => {
+   const [addsignup, {isLoading}] = useSignUpMutation();
+   const navigate = useNavigate();
+   const onFinish = (values: IUsers) => {
+    addsignup(values)
+        .unwrap()
+        .then(() => navigate("/signin"));
 }; return (
+    // andt
     <div className="h-screen font-sans bg-cover bg-no-repeat bg-[url('https://khoinguonsangtao.vn/wp-content/uploads/2022/11/hinh-anh-binh-minh.jpg')]">
-           <header className="mb-4">
-                <h2 className="font-bold text-2xl text-center">Đăng nhập</h2>
+       <header className="mb-4">
+                <h2 className="font-bold text-2xl text-center">Đăng Ký</h2>
             </header>
-    <Form
-    name="basic"
-    className='container mx-auto h-full'
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Email"
-      name="email"
-      rules={[{ required: true, message: 'Bắt buộc phải nhập Email!' }]}
-    >
-      <Input />
-    </Form.Item>
+        <Form
+          className='container mx-auto h-full'
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >   
+          <Form.Item<FieldType>
+            label="Tên Người Dùng"
+            name="name"
+            rules={[{ required: true, message: 'Không được để trống' }]}
+          >
+            <Input />
+          </Form.Item>
 
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Hãy nhập mật khẩu' }]}
-    >
-      <Input.Password />
-    </Form.Item>
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-    <Button type="primary" danger htmlType="submit">
+          <Form.Item<FieldType>
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Không được để trống' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+        name="phoneNumber"
+        label="Số điện thoại"
+        rules={[{ required: true, message: 'Không được để trống số điện thoại!' }]}
+      >
+        <Input style={{ width: '100%' }} />
+      </Form.Item>
+
+          <Form.Item<FieldType>
+            label="Mật khẩu"
+            name="password"
+            rules={[{ required: true, message: 'Không được để trống' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            label="Nhập lại mật khẩu"
+            name="confirmPassword"
+            rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập lại mật khẩu!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(new Error('Mật khẩu mới mà bạn đã nhập không khớp!'));
+                  },
+                }),
+              ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" danger htmlType="submit">
                         {isLoading ? (
                             <AiOutlineLoading3Quarters className="animate-spin" />
                         ) : (
-                            "Đăng nhập"
+                            "Đăng ký"
                         )}
                     </Button>
-    </Form.Item>
-  </Form>
-  </div>
 
-        )
-    // return (
-    //     <>
-    //         <div
-    //             className="h-screen font-sans bg-cover bg-no-repeat bg-[url('https://khoinguonsangtao.vn/wp-content/uploads/2022/11/hinh-anh-binh-minh.jpg')]">
-    //             <div className="container mx-auto h-full flex flex-1 justify-center items-center">
-    //                 <div className="w-full max-w-lg">
-    //                     <div className="leading-loose">
-    //                         <form className="max-w-sm m-4 p-10 bg-white bg-opacity-30 rounded shadow-xl">
-    //                             <p className="text-black text-center text-lg font-bold">SIGN IN</p>
-    //                             <div className="">
-    //                                 <label className="block text-lg font-semibold text-black" htmlFor="email">E-mail</label>
-    //                                 <input
-    //                                     className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded focus:outline-none focus:bg-white"
-    //                                     type="email" placeholder="Nhập email của bạn" />
-    //                             </div>
-    //                             <div className="mt-2">
-    //                                 <label className="block  text-lg font-semibold text-black">Mật khẩu</label>
-    //                                 <input
-    //                                     className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded focus:outline-none focus:bg-white"
-    //                                     type="password" placeholder="Nhập mật khẩu của bạn" />
-    //                             </div>
-    //                             <div className="my-5">
-    //                                 <button className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 bg-gray-200 hover:bg-white hover:shadow transition duration-150">
-    //                                     <img src="https://www.svgrepo.com/show/355037/google.svg" className="w-6 h-6" alt="" /> <span>Login with Google</span>
-    //                                 </button>
-    //                             </div>
-    //                             <div className="mt-4 items-center justify-between space-x-5">
-    //                                 <button
-    //                                     className="px-4 py-1 text-white font-normal tracking-wider bg-blue-900 hover:bg-blue-500 rounded"
-    //                                     type="submit">Đăng nhập</button>
-
-    //                                 <Link to="/signup">
-    //                                     <button
-    //                                         className="px-4 py-1 text-white font-normal tracking-wider bg-blue-900 hover:bg-blue-500 rounded"
-    //                                         type="submit">Đăng ký
-    //                                     </button>
-    //                                 </Link>
-
-    //                                 <a className="inline-block items-center right-0 align-baseline font-bold text-sm text-500 text-black hover:text-gray-300"
-    //                                     href="#">Quên mật khẩu ?</a>
-    //                             </div>
-    //                             <div className="button-dangky">
-
-    //                             </div>
-    //                         </form>
-
-
-    //                     </div>
-    //                 </div>
-
-    //             </div>
-    //         </div>
-    //     </>
-    // )
+                </Form.Item>
+        </Form>
+        </div>
+)
 }
 
-export default Signin
+export default Signup
