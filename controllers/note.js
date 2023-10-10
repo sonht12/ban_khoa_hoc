cba7cd19fa8ef2cd54ed9bdecb618106e8887669
DuongLuc2003 <106455957+DuongLuc2003@users.noreply.github.com>
2023-10-10 20:03:34 +0700

    feat/NoteApi-refs-BKH-36

---
+++ controllers/note.js
@@ -0,0 +1,127 @@
+import Note from '../models/note'; // Import model Note
+import Product from '../models/product';
+import Lesson from '../models/lesson'
+import { noteSchema } from '../middlewares/note'
+// Controller để tạo ghi chú mới
+export const createNote = async (req, res) => {
+  try {
+    const { lessonId, title, content } = req.body;
+    // const user = req.user._id;
+    // Kiểm tra xem lessonId có tồn tại trong cơ sở dữ liệu không.
+    const existingLesson = await Lesson.findById(lessonId);
+    if (!existingLesson) {
+      return res.status(404).json({ error: 'Không tìm thấy bài học có lessonId tương ứng.' });
+    }
+
+    // Tạo một ghi chú mới và liên kết nó với bài học (lessonId).
+    const newNote = new Note({
+      lessonId,
+      title,
+      content,
+      // user
+    });
+    const savedNote = await newNote.save();
+    if (!savedNote) {
+      return res.status(500).json({ error: 'Lỗi khi lưu ghi chú.' });
+    }
+    res.status(201).json({ message: 'Ghi chú đã được tạo thành công.', data: savedNote });
+  } catch (error) {
+    console.error('Lỗi khi tạo ghi chú:', error);
+    res.status(500).json({ error: 'Lỗi khi tạo ghi chú.' });
+  }
+};
+
+
+
+
+  export const getAllNotes = async (req, res) => {
+    try {
+      const lessonId = req.params.lessonId;
+      // Tìm tất cả các ghi chú liên quan đến bài học có lessonId.
+      const notes = await Note.find({ lesson: lessonId });
+      res.status(200).json(notes);
+    } catch (error) {
+      console.error('Lỗi khi lấy danh sách ghi chú cho bài học:', error);
+      res.status(500).json({ error: 'Lỗi khi lấy danh sách ghi chú.' });
+    }
+  };
+
+  export const getNote = async (req, res) => {
+    try {
+      const noteId = req.params.id;
+      // Tìm ghi chú có ID là noteId.
+      const note = await Note.findById(noteId);
+  
+      if (!note) {
+        return res.status(404).json({ error: 'Không tìm thấy ghi chú.' });
+      }
+      res.status(200).json(note);
+    } catch (error) {
+      console.error('Lỗi khi lấy thông tin ghi chú:', error);
+      res.status(500).json({ error: 'Lỗi khi lấy thông tin ghi chú.' });
+    }
+  };
+
+
+  export const deleteNote = async (req, res) => {
+    try {
+      const noteId = req.params.id;
+      // Xóa ghi chú có ID là noteId.
+      const deletedNote = await Note.findByIdAndDelete(noteId);
+  
+      if (!deletedNote) {
+        return res.status(404).json({ error: 'Không tìm thấy ghi chú để xóa.' });
+      }
+      res.status(200).json({ message: 'Ghi chú đã được xóa thành công.' });
+    } catch (error) {
+      console.error('Lỗi khi xóa ghi chú:', error);
+      res.status(500).json({ error: 'Lỗi khi xóa ghi chú.' });
+    }
+  };
+
+  export const updateNote = async (req, res) => {
+    try {
+      const noteId = req.params.id;
+      const { title, content } = req.body;
+      // Cập nhật thông tin ghi chú có ID là noteId.
+      const updatedNote = await Note.findByIdAndUpdate(
+        noteId,
+        { title, content },
+        { new: true }
+      );
+      if (!updatedNote) {
+        return res.status(404).json({ error: 'Không tìm thấy ghi chú để cập nhật.' });
+      }
+      res.status(200).json(updatedNote);
+    } catch (error) {
+      console.error('Lỗi khi cập nhật ghi chú:', error);
+      res.status(500).json({ error: 'Lỗi khi cập nhật ghi chú.' });
+    }
+  };
+
+  export const getNotesByLessonId = async (req, res) => {
+  try {
+    const lessonId = req.params.lessonId;
+
+    // Tìm tất cả các ghi chú có lessonId tương ứng
+    const notes = await Note.find({ lessonId });
+
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error('Lỗi khi lấy danh sách ghi chú theo bài học:', error);
+    res.status(500).json({ error: 'Lỗi khi lấy danh sách ghi chú.' });
+  }
+};
+export const getNotesByUserId = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    // Tìm tất cả các ghi chú có userId tương ứng
+    const notes = await Note.find({ user: userId });
+
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error('Lỗi khi lấy danh sách ghi chú theo người dùng:', error);
+    res.status(500).json({ error: 'Lỗi khi lấy danh sách ghi chú.' });
+  }
+};
