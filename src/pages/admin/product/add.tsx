0cc1d85b44d6e706741a4dabacc2806db5e40193
truongbqph24877 <97102695+truong20033002@users.noreply.github.com>
2023-10-14 13:31:46 +0700

    feat/update_video_img-refs-BKH-39

--- src/pages/admin/product/add.tsx
+++ src/pages/admin/product/add.tsx
@@ -1,37 +1,38 @@
-import { useGetCategorysQuery } from "@/Api/categoryApi";
-import { Category } from "@/interface/categorys";
-import { useAddProductMutation } from "@/Api/productApi";
-import { IProduct } from "@/interface/products";
-import { Form, Button, Input, Select } from "antd";
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
-type FieldType = {
-    name: string;
-    price: number | string;
-    img: string;
-    description: string;
-    categoryId: string;
-    paymentContent: string | null
-};
+import React, { useState } from 'react';
+import { Form, Input, Button, Select } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { useAddProductMutation } from '@/Api/productApi';
+import { useGetCategorysQuery } from '@/Api/categoryApi';
+
 const Addproduct = () => {
     const [addProduct, { isLoading }] = useAddProductMutation();
     const navigate = useNavigate();
-    const onFinish = (values: IProduct) => {
-        addProduct(values)
+    const [selectedImageFile, setSelectedImageFile] = useState(null);
+
+    const onFinish = (values:any) => {
+        const formData:any = new FormData();
+        formData.append('name', values.name);
+        formData.append('price', values.price);
+        formData.append('img', selectedImageFile); // Thêm tệp ảnh vào formData
+        formData.append('description', values.description);
+        formData.append('categoryId', values.categoryId);
+        formData.append('paymentContent', values.paymentContent);
+          
+        console.log(formData);
+        
+        addProduct(formData)
             .unwrap()
-            .then(() => navigate("/admin/products"));
+            .then(() => navigate('/admin/products'));
+    };
+
+
+    const handleImageChange = (event:any) => {
+        const file = event.target.files[0];
+        setSelectedImageFile(file);
     };
 
     const { data: categoryData } = useGetCategorysQuery();
-    console.log(categoryData?.data);
-    const dataSource = categoryData?.data.map(({ _id, name }: Category) => ({
-        key: _id,
-        _id,
-        name,
-    }))
-
-
-
+    const numberPattern = /^[0-9]*$/; 
     return (
         <div>
             <header className="mb-4">
@@ -45,56 +46,29 @@ const Addproduct = () => {
                 onFinish={onFinish}
                 autoComplete="off"
             >
-                <Form.Item<FieldType>
-                    label="Tên khóa học"
-                    name="name"
-                    rules={[
-                        { required: true, message: "Vui lòng nhập tên khóa học!" },
-                        { min: 3, message: "khóa học ít nhất 3 ký tự" },
-                    ]}
-                >
+                <Form.Item label="Tên khóa học" name="name" rules={[{ required: true, message: 'Vui lòng nhập tên khóa học!' }, { min: 5, message: 'Khóa học ít nhất 3 ký tự' }]}>
                     <Input />
                 </Form.Item>
 
-                <Form.Item<FieldType> label="ảnh" name="img"
-                >
+                <Form.Item label="Ảnh" name="img">
+                    <input type="file" accept="image/*" onChange={handleImageChange} />
+                </Form.Item>
+
+                <Form.Item label="Giá khóa học" name="price" rules={[{ required: true, message: 'Vui lòng nhập giá khóa học!' }, { min: 5, message: 'Khóa học ít nhất 5 chữ số' }, { pattern: numberPattern, message: 'Chỉ được nhập số!' }, ]}>
                     <Input />
                 </Form.Item>
 
-
-                <Form.Item<FieldType> label="Giá khóa học" name="price"
-                    rules={[
-                        { required: true, message: "Vui lòng nhập giá khóa học!" },
-                        { min: 5, message: "khóa học ít nhất 5 chữ số" },
-
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item<FieldType> label="Nội Dung Thanh Thanh Toán" name="paymentContent"
-
-                >
+                <Form.Item label="Nội Dung Thanh Thanh Toán" name="paymentContent">
                     <Input />
                 </Form.Item>
 
-                <Form.Item<FieldType>
-                    label="mô tả"
-                    name="description"
-                    rules={[
-                        { required: true, message: "Vui lòng nhập mô tả!" },
-                        { min: 10, message: "khóa học ít nhất 10 ký tự" },
-                    ]}
-                >
+                <Form.Item label="Mô tả" name="description" rules={[{ required: true, message: 'Vui lòng nhập mô tả!' }, { min: 10, message: 'Khóa học ít nhất 10 ký tự' }]}>
                     <Input />
                 </Form.Item>
 
-                <Form.Item label="category" name="categoryId"
-                    rules={[
-                        { required: true, message: "Vui lòng nhập category!" },
-                    ]}
-                >
+                <Form.Item label="Category" name="categoryId" rules={[{ required: true, message: 'Vui lòng nhập category!' }]}>
                     <Select>
-                        {categoryData?.data.map(({ _id, name }: Category) => (
+                        {categoryData?.data.map(({ _id, name }) => (
                             <Select.Option key={_id} value={_id}>
                                 {name}
                             </Select.Option>
@@ -104,16 +78,11 @@ const Addproduct = () => {
 
                 <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                     <Button type="primary" danger htmlType="submit">
-                        {isLoading ? (
-                            <AiOutlineLoading3Quarters className="animate-spin" />
-                        ) : (
-                            "Thêm"
-                        )}
+                        {isLoading ? 'Đang tải lên...' : 'Thêm'}
                     </Button>
                     <Button
-
-                        className="ml-2  bg-yellow-500 text-white"
-                        onClick={() => navigate("/admin/products")}
+                        className="ml-2 bg-yellow-500 text-white"
+                        onClick={() => navigate('/admin/products')}
                     >
                         Quay lại
                     </Button>
