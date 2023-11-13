a9a013c2dbd55abde215d9cf209276bb8a9e8bc8
truongbqph24877 <97102695+truong20033002@users.noreply.github.com>
2023-11-13 10:25:25 +0700

    feat/update_tien_do_trang_thai_hoc-refs/BKH-41

--- controllers/Score.js
+++ controllers/Score.js
@@ -2,26 +2,24 @@ import learning_process from "../models/learning_process";
 import scoreSchemar from "../models/scoreSchemar";
 export const saveScore  = async (req, res) => {
   try {
-    const { score, lessonName,lessonId, progressId } = req.body;
+    const { score,statusVideo, lessonName,lessonId, progressId } = req.body;
     // Tìm bản ghi điểm số hiện tại dựa trên progressId và lessonName
-    const existingScore = await scoreSchemar.findOne({ progressId, lessonName , lessonId });
+    const existingScore = await scoreSchemar.findOne({ progressId, lessonId });
   
     // Kiểm tra xem bản ghi có tồn tại và liệu điểm số mới có cao hơn không
     if (existingScore && score > existingScore.score) {
       // Xác định trạng thái dựa trên điểm số mới
-      const status = score >= 80 ? 'hoàn thành' : 'chưa hoàn thành';
-      
       // Cập nhật bản ghi điểm số
       const updatedScore = await scoreSchemar.findByIdAndUpdate(
         existingScore._id,
-        { score, status },
+        { score},
         { new: true }
       );
       res.status(200).json(updatedScore);
     } else if (!existingScore) {
       // Nếu không tồn tại bản ghi, tạo mới
-      const status = score >= 80 ? 'hoàn thành' : 'chưa hoàn thành';
-      const newScore = new scoreSchemar({ score, lessonName, status, progressId, lessonId });
+ 
+      const newScore = new scoreSchemar({ score,statusVideo,lessonName,progressId, lessonId });
 
       await newScore.save();
 
@@ -43,6 +41,42 @@ export const saveScore  = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
   };
+
+  export const updatestatusVideo = async (req, res) => {
+    try {
+      // Sử dụng hàm getOne để lấy dữ liệu ban đầu
+      const dataToUpdate = await scoreSchemar.findById(req.params.id);
+  
+      if (!dataToUpdate) {
+        return res.status(404).json({
+          message: "Không tìm thấy bản ghi",
+        });
+      }
+  
+      // Kiểm tra trạng thái của bản ghi
+      if (dataToUpdate.statusVideo === "hoàn thành video") {
+        return res.status(400).json({
+          message: "Không thể cập nhật khi trạng thái đã 'hoàn thành'",
+        });
+      }
+  
+      // Nếu trạng thái không phải 'hoàn thành', thực hiện cập nhật
+      const updatedData = await scoreSchemar.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+  
+      return res.json({
+        message: "Cập nhật thành công",
+        data: updatedData,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: error.message,
+      });
+    }
+  };
   export const getOne=async(req,res)=>{
     try {
         const data = await scoreSchemar.findById(req.params.id);
@@ -56,3 +90,29 @@ export const saveScore  = async (req, res) => {
         })
     }
 }
+export const getAll= async (req, res, next) => {
+  try {
+    const data = await scoreSchemar.find();
+    return res.json(data);
+  } catch (error) {
+    return res.status(401).json({
+      message: error.message,
+      
+    });
+  }
+};
+export const getScoreForprogress = async (req, res, next) => {
+  try {
+    const progressId = req.params.progressId;
+    const data = await scoreSchemar.find({
+      progressId: progressId,
+      statusVideo: "hoàn thành video",
+      score: { $gt: 80 }, // Lọc theo điểm số lớn hơn 80
+    });
+    return res.json(data);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
