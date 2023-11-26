075b6ff116fbe0f283613a9ed0b01a3484858988
ngoductruongg <ngoductruongg@gmail.com>
2023-11-26 20:36:33 +0700

    feat/responesive_2-refs-BKH-45

--- src/components/Layouts/forgotPassword.tsx
+++ src/components/Layouts/forgotPassword.tsx
@@ -5,7 +5,8 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useForgotPasswordMutation, useResetPasswordMutation } from '@/Api/userApi';
-
+import { FaBarcode } from "react-icons/fa";
+import "./signin_signup.css"
 const ForgotPassword = () => {
   const [form] = Form.useForm();
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -58,62 +59,68 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="  flex justify-center">
-      <div  className="contaiiiner   ">
-      <div className="login-content ">
-            <div className="login-formmm">
-           
-    <Form
-      form={form}
-      name="forgot_password_form"
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-       <h2 className="form-title mr-10">Quên Mật khẩu</h2>
-      <Form.Item
-        name="email"
-        rules={[{ required: true, message: 'Bắt buộc phải nhập Email!' }]}
-      >
-        <Input
-          placeholder="Nhập email của bạn"
-          prefix={<BiLogoGmail />}
-          disabled={isSubmitting || otpSent}
-        />
-      </Form.Item>
-      {otpSent && (
-        <>
-          <Form.Item
-            name="otp"
-            rules={[{ required: true, message: 'Vui lòng nhập mã OTP!' }]}
-          >
-            <Input placeholder="Nhập mã OTP" disabled={isSubmitting} />
-          </Form.Item>
-          <Form.Item
-            name="newPassword"
-            rules={[{ required: true, message: 'Vui lòng nhập mật khẩu mới!' }]}
-          >
-            <Input.Password
-              placeholder="Nhập mật khẩu mới"
-              prefix={<BiLockAlt />}
-              disabled={isSubmitting}
-            />
-          </Form.Item>
-        </>
-      )}
-      <Form.Item>
-        <Button danger type="primary" htmlType="submit" loading={isSubmitting}>
-          {otpSent ? 'Đổi mật khẩu' : 'Lấy mã'}
-        </Button>
-      </Form.Item>
-    </Form>
-    <div className="login-image">
-               <img className="w-[400px] h-[300px] " src="../../../public/img/signin.jpg" alt="" />
-                <a href="/signup" className="login-image-link text-[18px]">Tạo tài khoản</a>
-                </div>
+    <div className="  lg:flex lg:justify-center">
+      <div  className="lg:w-[900px] h-[550px] mt-[50px] lg:flex justify-center text-center rounded-[20px] lg:shadow-custom ">
+      <div className="py-[75px] ">
+            <div className="lg:flex">
+
+            <Form
+              form={form}
+              name="forgot_password_form"
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              className='w-[100%] lg:w-[70%] px-6 lg:px-0'
+            >
+              <h2 className="lg:mr-10 text-[40px] mb-[33px]">Quên Mật khẩu</h2>
+              <Form.Item
+              className="relative mb-[25px]"
+                name="email"
+                rules={[{ required: true, message: 'Bắt buộc phải nhập Email!' }]}
+              >
+                <Input
+                 className='py-4 lg:py-0 input no-border-radius input-prefix-spacing w-full lg:w-[300px] lg:mr-[25px]'
+                 placeholder="Nhập email của bạn"
+                  prefix={<BiLogoGmail />}
+                  disabled={isSubmitting || otpSent}
+                />
+              </Form.Item>
+              {otpSent && (
+                <>
+                  <Form.Item
+                    name="otp"
+                    rules={[{ required: true, message: 'Vui lòng nhập mã OTP!' }]}
+                  >
+                    <Input
+             className='py-4 lg:py-0 input no-border-radius input-prefix-spacing w-full lg:w-[300px] lg:mr-[25px]'
+             prefix={<FaBarcode  />} placeholder="Nhập mã OTP" disabled={isSubmitting} />
+                  </Form.Item>
+                  <Form.Item
+                    name="newPassword"
+                    rules={[{ required: true, message: 'Vui lòng nhập mật khẩu mới!' }]}
+                  >
+                    <Input.Password
+                     className='py-4 lg:py-0 input no-border-radius input-prefix-spacing w-full lg:w-[300px] lg:mr-[25px]'
+                     placeholder="Nhập mật khẩu mới"
+                      prefix={<BiLockAlt />}
+                      disabled={isSubmitting}
+                    />
+                  </Form.Item>
+                </>
+              )}
+              <Form.Item>
+              <Button className='lg:mr-[45px] mt-6 text-[20px] w-[140px] h-[50px]' danger type="primary" htmlType="submit" loading={isSubmitting}>
+                  {otpSent ? 'Đổi mật khẩu' : 'Lấy mã'}
+                </Button>
+              </Form.Item>
+            </Form>
+            <div className="login-image">
+              <img className="w-[400px] h-[300px] hidden lg:block" src="../../../public/img/signin.jpg" alt="" />
+              <a href="/signup" className="login-image-link text-[18px]">Tạo tài khoản</a>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-  </div>
+    </div>
   );
 };
 
